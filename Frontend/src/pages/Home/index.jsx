350dd1eaa1f545fed3f3b6b9babb33a20a47f4b8
LayeNdiay <elhadjiabdoulayen69@gmail.com>
2023-01-26 12:42:41 +0000

    Timeline setp 1

--- Frontend/src/pages/Home/index.jsx
+++ Frontend/src/pages/Home/index.jsx
@@ -1,4 +1,4 @@
-import { useContext,useEffect,useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import styled from "styled-components";
 import colors from "../../utils/styles/colors";
 import Header from "../../components/Header";
@@ -10,114 +10,128 @@ import PostNew from "../../components/PostNew";
 import ModalHobbie from "../../components/ModalHobbie";
 import { AuthContext } from "../../utils/styles/Contexte";
 
-
 const Container = styled.div`
-    box-sizing:border-box;
-    position:relative;
-    display:flex;
-    flex-direction:column;
-    background:#263789;
+  box-sizing: border-box;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  background: #263789;
 `;
 
 const Body = styled.div`
-    position:relative;
-    padding-bottom:60px;
-    background-color: #D2E0F4;
-    z-index:1;
+  position: relative;
+  padding-bottom: 60px;
+  background-color: #d2e0f4;
+  z-index: 1;
 `;
 
 const TimeLine = styled.div`
-    height:600vh;    
-    max-width:35em;
-    position: relative;
-    top:80px;
-    left : 22.3em;  
-    margin: 20px 0;
-    top:80px;
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
+  height: 600vh;
+  max-width: 35em;
+  position: relative;
+  top: 80px;
+  left: 22.3em;
+  margin: 20px 0;
+  top: 80px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
 `;
 
 const LeftSidebar = styled.div`
-    height:500px;
-    position:fixed;
-    border-radius:20px;
-    left:0px;
-    top:80px;
-    margin: 20px;
-    width: 25%;
-    background: rgba(255,255, 255, 0.3);
+  height: 500px;
+  position: fixed;
+  border-radius: 20px;
+  left: 0px;
+  top: 80px;
+  margin: 20px;
+  width: 25%;
+  background: rgba(255, 255, 255, 0.3);
 `;
 const RightSidebar = styled.div`
-    height:500px;
-    position:fixed;
-    border-radius:20px;
-    top:80px;
-    right:0px;
-    margin:20px;
-    width: 25%;
-    background: rgba(255,255, 255, 0.3);
+  height: 500px;
+  position: fixed;
+  border-radius: 20px;
+  top: 80px;
+  right: 0px;
+  margin: 20px;
+  width: 25%;
+  background: rgba(255, 255, 255, 0.3);
 `;
 
-function Home(){
-    const [showModalQuestion, setShowModalQuestion] = useState(false);
-    const [showModalPost, setShowModalPost] = useState(false);
-    const [showModalHobbie, setShowModalHobbie] = useState(false);
-    
-    const handleQuestionClick= ()=>{
-        setShowModalQuestion(true);
-    }
-    const handlePostClick= ()=>{
-        setShowModalPost(true);
-        setShowModalHobbie(false);
-    }
-    const handleOverlayClick= ()=>{
-        setShowModalPost(false);
-        setShowModalQuestion(false);
-        setShowModalHobbie(false);
-    }
-    const handleCloseClick= ()=>{
-        setShowModalPost(false);
-        setShowModalQuestion(false);
-    }
+function Home() {
+  const [showModalQuestion, setShowModalQuestion] = useState(false);
+  const [showModalPost, setShowModalPost] = useState(false);
+  const [showModalHobbie, setShowModalHobbie] = useState(false);
 
-    const handleHobbieClick = ()=>{
-        setShowModalPost(false);
-        setShowModalQuestion(false);
-        setShowModalHobbie(true);
-    }
-    const handleHobbieClose = ()=>{ 
-        setShowModalHobbie(false);
-        setShowModalPost(false);
-        setShowModalQuestion(true);
-    }
+  const handleQuestionClick = () => {
+    setShowModalQuestion(true);
+  };
+  const handlePostClick = () => {
+    setShowModalPost(true);
+    setShowModalHobbie(false);
+  };
+  const handleOverlayClick = () => {
+    setShowModalPost(false);
+    setShowModalQuestion(false);
+    setShowModalHobbie(false);
+  };
+  const handleCloseClick = () => {
+    setShowModalPost(false);
+    setShowModalQuestion(false);
+  };
 
-    const handleHobbieSubmit = ()=>{
-        setShowModalPost(false);
-        setShowModalQuestion(true);
-        setShowModalHobbie(false);
-    }
+  const handleHobbieClick = () => {
+    setShowModalPost(false);
+    setShowModalQuestion(false);
+    setShowModalHobbie(true);
+  };
+  const handleHobbieClose = () => {
+    setShowModalHobbie(false);
+    setShowModalPost(false);
+    setShowModalQuestion(true);
+  };
 
-    return(
-        <div>
-            <Container>
-                <Header page={"home"}/>
-                {showModalQuestion && <QuestionNew onOverlayClick={handleOverlayClick} onCloselayClick={handleCloseClick} onHobbieClick={handleHobbieClick}/>}
-                {showModalPost && <PostNew onOverlayClick={handleOverlayClick} onCloselayClick={handleCloseClick} onHobbieClick={handleHobbieClick}/>}
-                {/* {showModalHobbie && <ModalHobbie onOverlayClick={handleHobbieClose} onCloselayClick={handleHobbieClose} onHobbieSubmit={handleHobbieSubmit} />} */}
-                <Body>
-                    <LeftSidebar></LeftSidebar>
-                    <TimeLine>
-                        <QuoiDeNeuf onQuestionClick={handleQuestionClick} onPublicationClick={handlePostClick} />
-                        <Publication/>
-                        <Question />
-                    </TimeLine>
-                    <RightSidebar></RightSidebar>
-                </Body>
-            </Container>
-        </div>
-    )
+  const handleHobbieSubmit = () => {
+    setShowModalPost(false);
+    setShowModalQuestion(true);
+    setShowModalHobbie(false);
+  };
+
+  return (
+    <div>
+      <Container>
+        <Header page={"home"} />
+        {showModalQuestion && (
+          <QuestionNew
+            onOverlayClick={handleOverlayClick}
+            onCloselayClick={handleCloseClick}
+            onHobbieClick={handleHobbieClick}
+          />
+        )}
+        {showModalPost && (
+          <PostNew
+            onOverlayClick={handleOverlayClick}
+            onCloselayClick={handleCloseClick}
+            onHobbieClick={handleHobbieClick}
+          />
+        )}
+        {/* {showModalHobbie && <ModalHobbie onOverlayClick={handleHobbieClose} onCloselayClick={handleHobbieClose} onHobbieSubmit={handleHobbieSubmit} />} */}
+        <Body>
+          <LeftSidebar></LeftSidebar>
+          <TimeLine>
+            <QuoiDeNeuf
+              onQuestionClick={handleQuestionClick}
+              onPublicationClick={handlePostClick}
+            />
+            <Publication />
+            <Question />
+          </TimeLine>
+          <RightSidebar></RightSidebar>
+        </Body>
+      </Container>
+    </div>
+  );
 }
 
 export default Home;
